7fb3a71f005773d66489bbfb944cfbb66a5e184d
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2022-05-19 13:02:18 +0200

    Fix incomplete element type for `struct timespec`

--- src/ptfs.c
+++ src/ptfs.c
@@ -16,6 +16,13 @@
 #define _XOPEN_SOURCE 700
 #endif
 
+#define _XOPEN_SOURCE
+#if __STDC_VERSION__ >= 199901L
+#define _XOPEN_SOURCE 600
+#else
+#define _XOPEN_SOURCE 500
+#endif /* __STDC_VERSION__ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
